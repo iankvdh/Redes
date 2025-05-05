854ae6e674941eab7c2694c9cda0bb8e3f6be1a2
iankvdh <ivon@fi.uba.ar>
2025-05-05 13:02:47 -0300

    wip: separacion de simulacion de fragmentacion y pkg loss Co-authored-by: Juan Martin de la Cruz <jdelacruz@fi.uba.ar>

--- TP1/src/tests/simulation-fragmentation.py
+++ TP1/src/tests/simulation-fragmentation.py
@@ -6,6 +6,12 @@ from mininet.log import setLogLevel, info
 from mininet.link import TCLink
 from mininet.term import makeTerms
 
+MIN_MTU = 500
+MAX_MTU = 1500
+
+IP_SERVER = "192.168.1.2/24"
+IP_ROUTER = "192.168.1.1/24"
+IP_H2 = "192.168.2.2/24"
 
 class LinuxRouter(Node):
     """Router con capacidad de forwarding IP y MTU reducido"""
@@ -27,41 +33,43 @@ def fragTopology():
     net.addController("c0")
 
     info("*** Agregando Hosts\n")
-    h1 = net.addHost("h1", ip="192.168.1.2/24", defaultRoute="via 192.168.1.1")
-    h2 = net.addHost("h2", ip="192.168.2.2/24", defaultRoute="via 192.168.2.1")
+    server = net.addHost("server", ip=IP_SERVER, defaultRoute="via 192.168.1.1")
+    h2 = net.addHost("h2", ip=IP_H2, defaultRoute="via 192.168.2.1")
 
     info("*** Agregando Switches\n")
     s1 = net.addSwitch("s1")
     s2 = net.addSwitch("s2")
 
+
     info("*** Agregando Router\n")
-    router = net.addHost("r1", cls=LinuxRouter, ip="192.168.1.1/24")
+    router = net.addHost("r1", cls=LinuxRouter, ip=IP_ROUTER)
 
     info("*** Creando Links\n")
-    # Enlace normal entre h1 y s1
-    # h1 --1500-- s1 --1500-- r1 --600-- s2 --1500-- h2
-    net.addLink(h1, s1)
+    # Enlace normal entre server y s1 con perdida de paquetes al 10%
+    # server --1500-- s1 --600-- r1 --1500-- s2 --1500-- h2
+    net.addLink(server, s1, cls=TCLink, mtu=MAX_MTU)
 
-    net.addLink(s1, router, intfName2="r1-eth1", params2={"ip": "192.168.1.1/24"})
+    net.addLink(s1, router, intfName2="r1-etserver", params2={"ip": IP_ROUTER}, cls=TCLink, mtu=MIN_MTU)
 
     net.addLink(
-        router, s2, intfName1="r1-eth2", params1={"ip": "192.168.2.1/24"}
-    )  # ,               cls=TCLink, mtu=500
+        router, s2, intfName1="r1-eth2", params1={"ip": "192.168.2.1/24"}, cls=TCLink, mtu=MAX_MTU)                
 
-    # Enlace normal entre s2 y h2 con perdida de paquetes al 10%
-    net.addLink(s2, h2, loss=25)
 
-    h1.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1")
-    h2.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1")
+    # Enlace normal entre s2 y h2
+    net.addLink(s2, h2, cls=TCLink, mtu=MAX_MTU)
+
+
+    for host in (server, h2):
+        host.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1") # desactivar la detección de MTU en extremos → esto obliga a que se fragmente en lugar de reducir el tamaño de paquetes.
+
 
     info("*** Comenzando Red\n")
     net.start()
 
     # Configurar MTU en las interfaces del router
-    # router.cmd('ifconfig r1-eth2 mtu 500')
+    router.cmd('ifconfig r1-eth2 mtu 500') #
 
-    info("*** Comenzando terminales para h1 y h2 con xterm\n")
-    makeTerms([h1, h2], term="xterm")
+
 
     info("*** Comenzando CLI\n")
     CLI(net)
