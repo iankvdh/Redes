c0ca24f5c43a47f18266107c83c0b472fefc867e
AgustinAltamirano <agustinga110704@gmail.com>
2025-05-05 22:40:35 -0300

    refactor: change fragmentation topology

--- TP1/src/tests/simulation-fragmentation.py
+++ TP1/src/tests/simulation-fragmentation.py
@@ -4,13 +4,7 @@ from mininet.node import OVSController, Node
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
-
-MIN_MTU = 500
-MAX_MTU = 1500
-
-IP_SERVER = "192.168.1.2/24"
-IP_ROUTER = "192.168.1.1/24"
-IP_H2 = "192.168.2.2/24"
+from mininet.term import makeTerms
 
 
 class LinuxRouter(Node):
@@ -33,50 +27,59 @@ def fragTopology():
     net.addController("c0")
 
     info("*** Agregando Hosts\n")
-    server = net.addHost("server", ip=IP_SERVER, defaultRoute="via 192.168.1.1")
-    h2 = net.addHost("h2", ip=IP_H2, defaultRoute="via 192.168.2.1")
+    h1 = net.addHost("h1", ip="192.168.0.2/30", defaultRoute="via 192.168.0.1")
+    h2 = net.addHost("h2", ip="192.168.0.6/30", defaultRoute="via 192.168.0.5")
 
     info("*** Agregando Switches\n")
     s1 = net.addSwitch("s1")
     s2 = net.addSwitch("s2")
 
     info("*** Agregando Router\n")
-    router = net.addHost("r1", cls=LinuxRouter, ip=IP_ROUTER)
+    router = net.addHost("r1", cls=LinuxRouter, ip="192.168.0.1/30")
 
     info("*** Creando Links\n")
-
-    net.addLink(server, s1, cls=TCLink, mtu=MAX_MTU)
+    net.addLink(h1, s1, intfName1="h1-eth1", intfName2="s1-eth1", mtu=1500)
 
     net.addLink(
         s1,
         router,
-        intfName2="r1-etserver",
-        params2={"ip": IP_ROUTER},
-        cls=TCLink,
-        mtu=MIN_MTU,
+        intfName1="s1-eth2",
+        intfName2="r1-eth1",
+        params2={"ip": "192.168.0.1/30"},
+        mtu=1500,
     )
 
     net.addLink(
         router,
         s2,
         intfName1="r1-eth2",
-        params1={"ip": "192.168.2.1/24"},
+        intfName2="s2-eth1",
+        params1={"ip": "192.168.0.5/30"},
         cls=TCLink,
-        mtu=MAX_MTU,
+        mtu=500,
     )
 
-    net.addLink(s2, h2, cls=TCLink, mtu=MAX_MTU, loss=10)  # Pérdida del 10%
+    # Enlace normal entre s2 y h2 con perdida de paquetes al 10%
+    net.addLink(s2, h2, intfName1="s2-eth2", intfName2="h2-eth1", loss=10, mtu=1500)
 
-    for host in (server, h2):
-        host.cmd(
-            "sysctl -w net.ipv4.ip_no_pmtu_disc=1"
-        )  # desactivar la detección de MTU en extremos
+    h1.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1")
+    h2.cmd("sysctl -w net.ipv4.ip_no_pmtu_disc=1")
+
+    # Disable TCP MTU probing
+    h1.cmd("sysctl -w net.ipv4.tcp_mtu_probing=0")
+    h2.cmd("sysctl -w net.ipv4.tcp_mtu_probing=0")
 
     info("*** Comenzando Red\n")
     net.start()
 
     # Configurar MTU en las interfaces del router
-    router.cmd(f"ifconfig r1-eth2 mtu {MIN_MTU}")
+    h1.cmd("ifconfig h1-eth1 mtu 1500")
+    h2.cmd("ifconfig h2-eth1 mtu 1500")
+    router.cmd("ifconfig r1-eth1 mtu 1500")
+    router.cmd("ifconfig r1-eth2 mtu 500")
+
+    info("*** Comenzando terminales para server y h2 con xterm\n")
+    makeTerms([h1, h2], term="xterm")
 
     info("*** Comenzando CLI\n")
     CLI(net)
