ebd050d30dedfe1c45552cf50cbebd67dca5d18c
AgustinAltamirano <agustinga110704@gmail.com>
2025-06-25 18:50:22 -0300

    refactor: log protocol name instead of code

--- TP2/firewall.py
+++ TP2/firewall.py
@@ -2,39 +2,41 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.addresses import EthAddr, IPAddr
 import pox.lib.packet as pkt
-from collections import namedtuple
 import os
 import json
 
+from protocol_codes import PROTOCOL_CODES
+
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ['HOME']
+policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ["HOME"]
 
 CONFIG_FILE = "rules.json"
 FIREWALL_SWITCH_ID = 3  # ID del switch donde aplicar firewall (s3)
 
 FIELD_MAP = {
-    "mac_src": "dl_src",            # Dirección MAC de origen
-    "mac_dst": "dl_dst",            # Dirección MAC de destino
-    "dl_type": "dl_type",           # Tipo de enlace de datos (por ejemplo, IPv4)
-    "dl_vlan": "dl_vlan",           # VLAN ID
-    "dl_vlan_pcp": "dl_vlan_pcp",   # Prioridad de VLAN
-    "nw_src": "nw_src",             # Dirección IP de origen
-    "nw_dst": "nw_dst",             # Dirección IP de destino
-    "nw_proto": "nw_proto",         # Protocolo de red (17 para UDP, 6 para TCP, 1 para ICMP)
-    "nw_tos": "nw_tos",             # Tipo de servicio de red
-    "tp_src": "tp_src",             # Puerto de origen (TCP/UDP)
-    "tp_dst": "tp_dst",             # Puerto de destino (TCP/UDP)
-    "in_port": "in_port",           # Puerto de entrada
+    "mac_src": "dl_src",  # Dirección MAC de origen
+    "mac_dst": "dl_dst",  # Dirección MAC de destino
+    "dl_type": "dl_type",  # Tipo de enlace de datos (por ejemplo, IPv4)
+    "dl_vlan": "dl_vlan",  # VLAN ID
+    "dl_vlan_pcp": "dl_vlan_pcp",  # Prioridad de VLAN
+    "nw_src": "nw_src",  # Dirección IP de origen
+    "nw_dst": "nw_dst",  # Dirección IP de destino
+    "nw_proto": "nw_proto",  # Protocolo de red (17 para UDP, 6 para TCP, 1 para ICMP)
+    "nw_tos": "nw_tos",  # Tipo de servicio de red
+    "tp_src": "tp_src",  # Puerto de origen (TCP/UDP)
+    "tp_dst": "tp_dst",  # Puerto de destino (TCP/UDP)
+    "in_port": "in_port",  # Puerto de entrada
 }
+
 
 class Firewall(object):
     def __init__(self):
-        
+
         dir_path = os.path.dirname(os.path.realpath(__file__))
         rules_path = os.path.join(dir_path, CONFIG_FILE)
-        with open(rules_path, 'r') as f:
+        with open(rules_path, "r") as f:
             self.rules = json.load(f)
-        
+
         for rule in self.rules:
             if "mac_src" in rule:
                 rule["mac_src"] = EthAddr(rule["mac_src"])
@@ -64,10 +66,11 @@ class Firewall(object):
             match = of.ofp_match()
 
             uses_ip_fields = any(
-                key in rule for key in ("nw_src", "nw_dst", "nw_proto", "tp_src", "tp_dst")
+                key in rule
+                for key in ("nw_src", "nw_dst", "nw_proto", "tp_src", "tp_dst")
             )
             if uses_ip_fields:
-                match.dl_type = pkt.ethernet.IP_TYPE # 0x0800 for IPv4
+                match.dl_type = pkt.ethernet.IP_TYPE  # 0x0800 for IPv4
 
             # Asignación dinámica de campos
             for rule_key, match_attr in FIELD_MAP.items():
@@ -83,10 +86,12 @@ class Firewall(object):
 
     def _handle_PacketIn(self, event):
         dpid = event.connection.dpid
-        packet = event.parsed.find('ipv4')
+        packet = event.parsed.find("ipv4")
         if not packet:
             return
-        log.info("Switch %s: %s %s -> %s", dpid, packet.protocol, packet.srcip, packet.dstip)
+        proto_name = PROTOCOL_CODES.get(packet.protocol, str(packet.protocol))
+        log.info("Switch %s: %s %s -> %s", dpid, proto_name, packet.srcip, packet.dstip)
+
 
 def launch():
     core.registerNew(Firewall)
